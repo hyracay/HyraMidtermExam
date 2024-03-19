aca71e15fb62e945d9bcf51989e179160962161b
Hyra Sabawil Cayambas <20198507@s.ubaguio.edu>
2024-03-20 00:31:29 +0800

    MARCH 30 12:31 AM

--- A/app.js
+++ A/app.js
@@ -7,3 +7,20 @@ hamburgerBtn.addEventListener("click", () => header.classList.toggle("show-mobil
 
 // Close mobile menu on close button click
 closeMenuBtn.addEventListener("click", () => hamburgerBtn.click());
+
+const questions= document.querySelectorAll('.question')
+questions.forEach((question) => {
+    console.log(question)
+    const btn = question.querySelector('.question-btn')
+    //btn event
+    btn.addEventListener('click', () => {
+        //one question at a time
+        questions.forEach((item) => {
+            if(item !== question){
+                item.classList.remove('show-text')
+            }
+        })
+        //show answer to question
+        question.classList.toggle('show-text')
+    })
+})
